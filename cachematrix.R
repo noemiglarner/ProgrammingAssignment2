525c2cfd7a5f41655cd2c4be3d845a9bba629a9b
Noemi Glarner <61805316+noemiglarner@users.noreply.github.com>
2020-05-24 09:24:08 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -30,7 +30,7 @@ cacheSolve <- function(x, ...) {
          i <- x$getinverse()
   if(!is.null(i)) {
     message("getting cached data")
-    return(9)
+    return(i)
   }
   data <- x$get()
   i <- solve(data, ...)
